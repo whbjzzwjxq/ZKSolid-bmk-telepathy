ab08056c0f498d2ecd2ae77a50662eb226d65b8f
jtguibas <jtguibas@gmail.com>
2022-11-15 19:45:43 +0000

    fix build scripts to have no comments

--- circuits/scripts/rotate/build.sh
+++ circuits/scripts/rotate/build.sh
@@ -18,48 +18,48 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-# echo "****COMPILING CIRCUIT****"
-# start=`date +%s`
-# echo "$CIRCUITS_DIR"/"$CIRCUIT_NAME".circom 
-# circom "$CIRCUITS_DIR"/"$CIRCUIT_NAME".circom --O1 --r1cs --sym --c --output "$BUILD_DIR"
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****COMPILING CIRCUIT****"
+start=`date +%s`
+echo "$CIRCUITS_DIR"/"$CIRCUIT_NAME".circom 
+circom "$CIRCUITS_DIR"/"$CIRCUIT_NAME".circom --O1 --r1cs --sym --c --output "$BUILD_DIR"
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
-# echo "****Running make to make witness generation binary****"
-# start=`date +%s`
-# make -C "$OUTPUT_DIR"
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****Running make to make witness generation binary****"
+start=`date +%s`
+make -C "$OUTPUT_DIR"
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
-# echo "****Executing witness generation****"
-# start=`date +%s`
-# "$OUTPUT_DIR"/"$CIRCUIT_NAME" input.json "$OUTPUT_DIR"/witness.wtns
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****Executing witness generation****"
+start=`date +%s`
+"$OUTPUT_DIR"/"$CIRCUIT_NAME" input.json "$OUTPUT_DIR"/witness.wtns
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
-# echo "****Converting witness to json****"
-# start=`date +%s`
-# npx snarkjs wej "$OUTPUT_DIR"/witness.wtns "$OUTPUT_DIR"/witness.json
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****Converting witness to json****"
+start=`date +%s`
+npx snarkjs wej "$OUTPUT_DIR"/witness.wtns "$OUTPUT_DIR"/witness.json
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
-# echo "****GENERATING ZKEY 0****"
-# start=`date +%s`
-# $NODE_CMD $SNARKJS_PATH zkey new "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$OUTPUT_DIR"/p1.zkey
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****GENERATING ZKEY 0****"
+start=`date +%s`
+$NODE_CMD $SNARKJS_PATH zkey new "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$OUTPUT_DIR"/p1.zkey
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
-# echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
-# start=`date +%s`
-# $NODE_CMD $SNARKJS_PATH zkey contribute "$OUTPUT_DIR"/p1.zkey "$OUTPUT_DIR"/p2.zkey -n="First phase2 contribution" -e="some random text for entropy"
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
+start=`date +%s`
+$NODE_CMD $SNARKJS_PATH zkey contribute "$OUTPUT_DIR"/p1.zkey "$OUTPUT_DIR"/p2.zkey -n="First phase2 contribution" -e="some random text for entropy"
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
-# echo "****VERIFYING FINAL ZKEY****"
-# start=`date +%s`
-# $NODE_CMD $SNARKJS_PATH zkey verify "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$OUTPUT_DIR"/p2.zkey
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+echo "****VERIFYING FINAL ZKEY****"
+start=`date +%s`
+$NODE_CMD $SNARKJS_PATH zkey verify "$BUILD_DIR"/"$CIRCUIT_NAME".r1cs "$PHASE1" "$OUTPUT_DIR"/p2.zkey
+end=`date +%s`
+echo "DONE ($((end-start))s)"
 
 echo "****EXPORTING VKEY****"
 start=`date +%s`
